ccae0eccd9419528166bfb7c62aa3b09534292da
guptamarcos <guptamarcos.official@gmail.com>
2026-07-26 17:44:29 +0530

    deploy the db

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -3,10 +3,10 @@ const ExpressError = require("../utils/ExpressError.js");
 
 async function connectDb() {
   try {
-    if (!process.env.MONGO_DB_URL) {
+    if (!process.env.ATLAS_DB_URL) {
       throw new ExpressError(500, "Database url not exist !!");
     }
-    await mongoose.connect(process.env.MONGO_DB_URL);
+    await mongoose.connect(process.env.ATLAS_DB_URL);
   } catch (err) {
     throw new ExpressError(err.statue, err.message);
   }
